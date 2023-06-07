7c06195a592a38bb0b339299b463816e8a2e6fac
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2023-06-07 19:08:32 -0300

    Commit-07-06

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,10 @@ module.exports = {
   dialect: "postgres",
   host: "localhost",
   username: "postgres",
+  password: "postgres",
   database: "codeburger",
   define: {
-    timespamps: true,
+    timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
--- src/database/migrations/20230602174138-create-users.js
+++ src/database/migrations/20230602174138-create-users.js
@@ -19,7 +19,7 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
-      password: {
+      password_hash: {
         type: Sequelize.STRING,
         allowNull: false,
       },
