46e6eb7276d18fe2ef157e66c9e2e2be0dbde8e6
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2023-06-30 21:48:23 -0300

    New-commit-30-06

--- src/database/migrations/20230630210148-create-categories.js
+++ src/database/migrations/20230630210148-create-categories.js
@@ -1,28 +1,24 @@
-'use strict';
+"use strict"
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    
-     * await queryInterface.createTable('categories', {
-       id: {
-        type:Sequelize.INTEGER,
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("categories", {
+      id: {
+        type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-       },
-       name: {
-        type:Sequelize.STRING,
+      },
+      name: {
+        type: Sequelize.STRING,
         allowNull: false,
-        unique: true
-       }
-      });
-  
+        unique: true,
+      },
+    })
   },
 
-  async down (queryInterface) {
- 
-     * await queryInterface.dropTable('categories');
-     
-  }
-};
+  async down(queryInterface) {
+    await queryInterface.dropTable("categories")
+  },
+}
